57af35a7bbd3bf1a7568a00debcabf46b187b6a9
krogank9 <wifi.mouse.app@gmail.com>
2019-09-26 20:58:40 -0400

    update for prod

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require('cors')
 const helmet = require('helmet')
 const MOVIES = require('./movies-data-small.json')
 
-console.log(process.env.API_TOKEN)
-
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+
 app.use(helmet())
 app.use(cors())
 
@@ -17,8 +17,6 @@ function validateBearerToken(req, res, next) {
 	
 	const authToken = req.get("Authorization")
 	const apiToken = process.env.API_TOKEN
-
-	console.log('validate bearer token middleware')
 
 	if(!authToken || authToken.split(' ')[1] !== apiToken) {
 		return res.status(401).json({ error: 'Unauthorized request' })
@@ -44,8 +42,18 @@ function handleGetMovie(req, res) {
 
 app.get('/movie', handleGetMovie)
 
-const PORT = 8000
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
 })
